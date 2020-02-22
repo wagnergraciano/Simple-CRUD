a727897a4e80c3321e2d2a68afc4d6b8155b1136
wagnergraciano <wagner_graciano0101@hotmail.com>
2020-02-22 12:37:46 -0300

    defining layout

--- Frontend/index.php
+++ Frontend/index.php
@@ -4,25 +4,40 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="./assest/style.scss">
+<link rel="stylesheet" type="text/css" href="./assets/style.scss">
+<link rel="stylesheet" type="text/css" href="./assets/layout.scss">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <title>CRUD simples</title>
 </head>
 <body>
-    <div class="card" style="margin:30px 200px 0 200px;">
-        <div class="card-header pesq">
-            Quote
-        </div>
-        <div class="card-body">
-            <blockquote class="blockquote mb-0">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-            </blockquote>
-        </div>
-        <div class="card-footer">
-            Quote
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-0 col-md-1"></div>
+    <div class="col-12 col-md-10">
+        <div class="card">
+            <div id="cabe" class="card-header">
+                Cadastro
+            </div>
+            <div class="card-body">
+                afs
+                <blockquote class="blockquote mb-0">
+                <footer class="footer"><div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <div class="input-group-text">
+    procurar
+    </div>
+  </div>
+  <input type="text" class="form-control" aria-label="Text input with checkbox">
+</div></footer>
+                </blockquote>
+            </div>
         </div>
     </div>
+    <div class="col-0 col-md-1"></div>
+  </div>
+</div>
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
